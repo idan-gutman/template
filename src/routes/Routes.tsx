8163a42b5f6e0bd44484328d6df5dfee44e56b5b
gute1993-idan <igutman93@gmail.com>
2023-08-16 14:42:16 +0300

    finished

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,8 +3,10 @@ import Path from "./paths";
 import Layout from "../components/Layout/Layout";
 import Generator from "../components/screens/generator/Generator";
 import Favorites from "../components/screens/favorites/Favorites";
+import { Navigate } from "react-router-dom";
 const Routes = () => {
   const routes: RouteObject[] = [
+    { path: Path.App, element: <Navigate replace to={Path.Generator} /> },
     {
       path: Path.App,
       element: <Layout />,
